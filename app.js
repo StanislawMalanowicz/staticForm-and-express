921c564e93226542694f6d735b369e8801f61e2f
Your Name <you@example.com>
2020-04-06 16:26:53 +0200

    bootstrap added

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const path = require('path')
 
 app.use(morgan('tiny'))
 app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
 app.use(bodyParser.urlencoded({ extended: false }));
 // console.log("path: ",path);
 app.get('/', function(req, res){
