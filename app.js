6240cea61af2c9b611869c54d1aed54a46690eaf
Your Name <you@example.com>
2020-04-07 12:37:08 +0200

    eslind and nodemon added

--- app.js
+++ app.js
@@ -1,30 +1,41 @@
-const express = require("express");
+const express = require('express');
+
 const app = express();
-const chalk = require("chalk");
-const bodyParser = require("body-parser");
+const chalk = require('chalk');
+const bodyParser = require('body-parser');
 const debug = require('debug')('app');
 const morgan = require('morgan');
-const path = require('path')
+const path = require('path');
 
-app.use(morgan('tiny'))
+app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '/public/')));
-app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
-app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')));
-app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.use(
+  '/css',
+  express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')),
+);
+app.use(
+  '/js',
+  express.static(path.join(__dirname, '/node_modules/bootstrap/dist/js')),
+);
+app.use(
+  '/js',
+  express.static(path.join(__dirname, '/node_modules/jquery/dist')),
+);
 app.use(bodyParser.urlencoded({ extended: false }));
 // console.log("path: ",path);
-app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname, '/views/', '/index.html'))
-})
-
-app.post("/submit-form", (req, res) => {
-  const username = req.body.username;
-  //...
-  console.log('data: ', username)
-//   res.end();
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '/views/', '/index.html'));
 });
 
-app.listen(3000, function(){
-    
-    console.log(chalk.blue("server is working at ", chalk.bgRed("https://localhost:3000")));
+app.post('/submit-form', (req, res) => {
+  const { username } = req.body;
+  // ...
+  console.log('data: ', username);
+  //   res.end();
+});
+
+app.listen(3000, () => {
+  console.log(
+    chalk.blue('server is working at ', chalk.red('https://localhost:3000')),
+  );
 });
